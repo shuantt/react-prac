8ad65cf593662f7bfa80715c6d504b5afc72cb58
MW\tehsuan.tseng <tehsnau.tseng@macrowell.com.tw>
2024-10-14 14:04:55 +0800

    feat: add table and pagination example
    - fetch data
    - table component
    - pagination
    - setup tailwindcss with prettier
    
    issue: week2-hw

--- src/App.jsx
+++ src/App.jsx
@@ -3,109 +3,146 @@ import { useEffect } from "react";
 import { useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
+import "./assets/css/index.css";
 
 function App() {
-  const StateComponent = () => {
-    let [count, setCount] = useState(0);
-    useEffect(() => {
-      // console.log("useEffect called in StateComponent");
-      // console.log("count 變動，count:" + count);
-    }, [count]);
+  const [data, setData] = useState([]);
+  const [curPage, setCurPage] = useState(1);
 
-    const normalAddCount = () => {
-      count++;
-      console.log("normalAddCount called, count: ", count);
-    }
+  const pageSize = 5;
+  const totalPages = Math.ceil(data.length / pageSize);
+  const startIndex = (curPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const curPageData = data.slice(startIndex, endIndex);
 
+  // 初始化取得資料
+  useEffect(() => {
+    const apiUrl = `https://jsonplaceholder.typicode.com/users`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const GridComponent = () => {
     return (
       <>
-        <h3>This is StateComponent</h3>
-        <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Add count</button>
-        <button onClick={normalAddCount}>直接+1</button>
-        <hr></hr>
+        <div className="overflow-x-auto p-4">
+          <table className="mx-auto border border-black text-center">
+            <thead>
+              <tr className="border-b-[1px] border-solid border-black">
+                <th className="border-r-[1px] border-black p-2">ID</th>
+                <th className="border-r-[1px] border-black p-2">Name</th>
+                <th className="border-r-[1px] border-black p-2">UserName</th>
+                <th className="border-r-[1px] border-black p-2">Email</th>
+                <th className="border-r-[1px] border-black p-2">Address</th>
+                <th className="border-r-[1px] border-black p-2">Phone</th>
+                <th className="border-r-[1px] border-black p-2">Website</th>
+                <th className="border-r-[1px] border-black p-2">Company</th>
+                <th className="border-r-[1px] border-black p-2">Phrase</th>
+                <th className="border-r-[1px] border-black p-2">Bussiness</th>
+              </tr>
+            </thead>
+            <tbody>
+              {curPageData.map((user) => (
+                <tr
+                  className="border-b-[1px] border-solid border-black"
+                  key={user.id}
+                >
+                  <td className="border-r-[1px] border-black p-2">{user.id}</td>
+                  <td className="border-r-[1px] border-black p-2">
+                    {user.name}
+                  </td>
+                  <td className="border-r-[1px] border-black p-2">
+                    {user.username}
+                  </td>
+                  <td className="border-r-[1px] border-black p-2">
+                    {user.email}
+                  </td>
+                  <td className="border-r-[1px] border-black p-2">{`${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}`}</td>
+                  <td className="border-r-[1px] border-black p-2">
+                    {user.phone}
+                  </td>
+                  <td className="border-r-[1px] border-black p-2">
+                    {user.website}
+                  </td>
+                  <td className="border-r-[1px] border-black p-2">
+                    {user.company.name}
+                  </td>
+                  <td className="border-r-[1px] border-black p-2">
+                    {user.company.catchPhrase}
+                  </td>
+                  <td className="border-r-[1px] border-black p-2">
+                    {user.company.bs}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </>
     );
   };
 
-  const RefComponent = () => {
-    const [count, setCount] = useState(1);
-    const countRef = useRef(5);
-    const addCount = () => {
-      // console.log("addCount called");
+  const PaginationComponent = () => {
+    const nextPage = () => {
+      if (curPage < totalPages) {
+        setCurPage(curPage + 1);
+      } else {
+      }
+    };
 
-      // 目前兩個都寫，因為 count 是 state，會重新渲染，因此 countRef.current 也會被重新渲染
-      // countRef 的生命週期會跟著 component 的生命週期，因此重新渲染後，值會一直被記錄
-      setCount(count + 1);
-      countRef.current += 1;
+    const lastPage = () => {
+      if (curPage > 1) {
+        setCurPage(curPage - 1);
+      } else {
+      }
+    };
 
-      // 這樣寫，countRef.current 的值會變動，但畫面不會被重新渲染
-      // countRef.current += 1
-
-      // 這樣寫 count 改變，countRef.current 會一直維持 5，不會變動
-      // setCount(count + 1)
-
-      // console.log("count: " + count);
-      // console.log("countRef: " + countRef.current);
+    const clickPageNo = (pageNo) => {
+      setCurPage(pageNo);
     };
     return (
-      <>
-        <h3>This is RefComponent</h3>
-        <p>Count: {count}</p>
-        <p>countRef: {countRef.current}</p>
-        <button onClick={addCount}>Add count</button>
-        <hr></hr>
-      </>
-    );
-  };
-
-  // const [userData, setUserData] = useState([]);
-  const userData = [];
-
-  useEffect(() => {
-    console.log("useEffect called in App", userData);
-    // const data = [];
-    // data.push({ name: "John", age: 20 });
-    // data.push({ name: "Tom", age: 25 });
-    // data.push({ name: "Jane", age: 30 });
-    // setUserData(data);
-    // userData.push({ name: "John", age: 20 });
-    // userData.push({ name: "Tom", age: 25 });
-    // userData.push({ name: "Jane", age: 30 });
-  }, [userData]);
-
-  let newId = useRef(0);
-  const addUser = () => {
-    console.log('addUser called');
-    newId.current++;
-    userData.push({ id: newId.current, name: "New User", age: 0 });
-    console.log(userData);
-    const updatedUserData = [...userData, { id: newId.current, name: "New User", age: 0 }];
-
-    // setUserData(() => { return [...userData, { id:newId.current , name: "New User", age: 0 }] });
-  }
-
-  const MapComponent = ({ user }) => {
-    return (
-      <>
-        <h3>
-          Name: {user.name}, Age: {user.age}
-        </h3>
-      </>
+      <div className="mx-auto flex justify-center gap-2 p-4">
+        <button
+          className="inline-block rounded border-[1px] border-solid border-black px-4 py-2 text-black hover:border-gray-500 hover:text-gray-500 disabled:cursor-not-allowed disabled:border-gray-400 disabled:text-gray-400"
+          onClick={lastPage}
+          disabled={curPage === 1}
+        >
+          上一頁
+        </button>
+        {Array.from({ length: totalPages }, (v, i) => (
+          <button
+            className={`inline-block rounded border-[1px] border-solid border-black px-4 py-2 hover:border-gray-500 hover:text-gray-500 ${curPage === i + 1 ? "bg-black text-white" : "bg-white text-black"} `}
+            key={i}
+            onClick={() => clickPageNo(i + 1)}
+          >
+            {i + 1}
+          </button>
+        ))}
+        <button
+          className="inline-block rounded border-[1px] border-solid border-black px-4 py-2 text-black hover:border-gray-500 hover:text-gray-500 disabled:cursor-not-allowed disabled:border-gray-400 disabled:text-gray-400"
+          onClick={nextPage}
+          disabled={curPage === totalPages}
+        >
+          下一頁
+        </button>
+      </div>
     );
   };
 
   return (
     <>
-      <StateComponent />
-      <RefComponent />
-      <h3>This is UserData List</h3>
-      {userData.map((user) => (
-        <MapComponent key={user.id} {...{ user }} />
-      ))}
-      <button onClick={addUser}>Add User</button>
-      <hr></hr>
+      <div className="mx-auto max-w-7xl">
+        <GridComponent />
+        <PaginationComponent />
+      </div>
     </>
   );
 }
